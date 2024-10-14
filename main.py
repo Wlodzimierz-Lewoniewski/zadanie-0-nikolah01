40ed16391c16e3ef1ff602c843d299ef75591613
Nikola Hrabska <83529@student.ue.poznan.pl>
2024-10-14 10:32:49 +0200

    Fixed code

--- main.py
+++ main.py
@@ -1,54 +1,38 @@
-
 import string
-from collections import defaultdict
-
-documents = [
-    "Your care set up, do not pluck my care down.",
-    "My care is loss of care with old care done.",
-    "Your care is gain of care when new care is won."
-]
 
 def preprocess_text(text):
-    punc = string.punctuation
-    s = list(text)
-    clean_text = "".join([char for char in s if char not in punc])
-    return clean_text.lower().split()
+    text = text.lower()
+    for char in string.punctuation:
+        text = text.replace(char, '')
+    return text.split()
 
 def count_word_occurrences(word, documents):
-    word_counts = defaultdict(int)
+    word = word.lower()
+    result = []
 
     for idx, doc in enumerate(documents):
         words = preprocess_text(doc)
-        count = sum(1 for w in words if w == word)
+        count = words.count(word)
+
         if count > 0:
-            word_counts[idx] = count
+            result.append((idx, count))
+    result.sort(key=lambda x: (-x[1], x[0]))
 
-    sorted_docs = sorted(word_counts.items(), key=lambda x: x[1], reverse=True)
+    return [doc[0] for doc in result]
 
-    return [doc[0] for doc in sorted_docs]
+def search_documents(documents, queries):
+    for query in queries:
+        result = count_word_occurrences(query, documents)
+        print(result)
 
-texts = [[word for word in preprocess_text(document)] for document in documents]
+if __name__ == "__main__":
+    ld = int(input("PODAJ LICZBĘ DOKUMENTÓW: "))
 
-replacements = (', ', '-', '!', '?')
-for doc in documents:
-    my_str = doc
-    for r in replacements:
-        my_str = my_str.replace(r, ' ')
-    words_replaced = my_str.split()
+    documents = [input(f"DOKUMENT {i + 1}: ").strip() for i in range(ld)]
 
-frequency = defaultdict(int)
-for text in texts:
-    for token in text:
-        frequency[token] += 1
+    lz = int(input("PODAJ LICZBĘ ZAPYTAŃ: "))
 
-sorted_frequency = sorted(frequency.items(), key=lambda x: x[1], reverse=True)
+    queries = [input(f"ZAPYTANIE {i + 1}: ").strip().lower() for i in range(lz)]
 
-for _ in range(1):
-    care_order = count_word_occurrences("care", documents)
-    print("Dokumenty posortowane według częstotliwości występowania słowa 'care':", care_order)
-
-    is_order = count_word_occurrences("is", documents)
-    print("Dokumenty posortowane według częstotliwości występowania słowa 'is':", is_order)
-
-
+    search_documents(documents, queries)
 
